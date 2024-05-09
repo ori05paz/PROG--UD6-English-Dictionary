48658a02c5acd506e9fd97dec1af5a042bd94d42
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-05-09 09:01:19 +0100

    feat(Menu): Creación del método showMenu en la clase Menu

---
+++ src/company/duolingo/util/Menu.java
@@ -0,0 +1,16 @@
+package company.duolingo.util;
+
+
+
+public class Menu{
+
+    public static int showMenu() {
+        String menu = "Menú Principal\n\n"
+                    + "1. Añadir palabra\n"
+                    + "2. Eliminar palabra\n"
+                    + "3. Existe palabra\n"
+                    + "4. Mostrar iniciales disponibles\n"
+                    + "5. Ver palabras por inicial\n"
+                    + "6. Cerrar programa";
+    }
+}
