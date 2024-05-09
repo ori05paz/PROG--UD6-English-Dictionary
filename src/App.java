0caefcf1fa4be23d6bc4606b39c34c9b32381c69
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-05-09 12:40:35 +0100

    feat(App): Creación de la clase ejecutable

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
+import company.duolingo.elements.Dictionary;
+import company.duolingo.util.Menu;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int option;
+        Dictionary dictionary = new Dictionary();
+        do {
+            option = Menu.showMenu();
+                switch (option) {
+                    case 1:
+                        dictionary.addWord();
+                        break;
+                    case 2:
+                        dictionary.deleteWord();
+                        break;
+                    case 3:
+                        dictionary.wordExist();
+                        break;
+                    case 4:
+                        dictionary.availableInitials();
+                        break;
+                    case 5:
+                        dictionary.wordByInitials();
+                        break;
+                    case 6:
+                        System.out.println("Saliendo del programa...");
+                        break;    
+                
+                    default:
+                        System.out.println("Opción inválida. Elige una opción válida");
+                        break;
+                }
+        } while (option != 6);
     }
 }
