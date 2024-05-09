482ff69571a42f29e44d18468a03de8d8bd881f9
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-05-09 09:18:26 +0100

    feat(Dictionary): Correción en la tabulación del método addWord

--- src/company/duolingo/elements/Dictionary.java
+++ src/company/duolingo/elements/Dictionary.java
@@ -12,24 +12,24 @@ public class Dictionary {
     Map<String, Set<String>> dictionary = new HashMap<>();
 
     public void addWord() {
-    String newWord = JOptionPane.showInputDialog(null, "Introduce una nueva palabra", "Nueva Palabra", 0);
+        String newWord = JOptionPane.showInputDialog(null, "Introduce una nueva palabra", "Nueva Palabra", 0);
     
-    String initial = newWord.substring(0, 1);
+        String initial = newWord.substring(0, 1);
 
-    if (this.dictionary.containsKey(initial)) {
-        Set<String> wordSet = this.dictionary.get(initial);
-        if (wordSet.add(newWord)) {
-            JOptionPane.showMessageDialog(null, "Palabra añadida correctamente.");
+        if (this.dictionary.containsKey(initial)) {
+            Set<String> wordSet = this.dictionary.get(initial);
+            if (wordSet.add(newWord)) {
+                JOptionPane.showMessageDialog(null, "Palabra añadida correctamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "La palabra ya está en el diccionario.");
+            }
+            this.dictionary.put(initial, wordSet);
         } else {
-            JOptionPane.showMessageDialog(null, "La palabra ya está en el diccionario.");
+            Set<String> newSet = new HashSet<>();
+            newSet.add(newWord);
+            this.dictionary.put(initial, newSet);
+            JOptionPane.showMessageDialog(null, "Palabra añadida correctamente.");
         }
-        this.dictionary.put(initial, wordSet);
-    } else {
-        Set<String> newSet = new HashSet<>();
-        newSet.add(newWord);
-        this.dictionary.put(initial, newSet);
-        JOptionPane.showMessageDialog(null, "Palabra añadida correctamente.");
     }
-}
     
 }
